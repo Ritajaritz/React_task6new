90ad5e9b53f1fe258806e531e5c56c191f72a4c0
ritaj <ritaj@RitajaDey2>
2023-10-14 22:56:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header>
+        <h1>Your Name</h1>
+        <p>Ritaja Dey</p>
       </header>
+      <section>
+        <h2>About Me</h2>
+        <p>I am a Web Development Enthuciast and currently pursuing 3rd Btech Degree in Vellore Institute Of Technology. I live in West Benghal, kolkata. I have deep interest in coding and hope to build great websites </p>
+      </section>
+      <section>
+        <h2>Projects</h2>
+        <div className="project">
+          <h3>Project 1</h3>
+          <p>Description of Project 1</p>
+        </div>
+        <div className="project">
+          <h3>Project 2</h3>
+          <p>Description of Project 2</p>
+        </div>
+      </section>
+      <section>
+        <h2>Contact</h2>
+        <p>Email: your.email@example.com</p>
+        <p>LinkedIn: linkedin.com/in/your-profile</p>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
